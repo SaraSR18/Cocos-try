f8f34e6a1760ad65e17f8b0398204b22d6ce3bcc
SaraSR18 <48553318+SaraSR18@users.noreply.github.com>
2021-05-31 17:02:39 +0200

    one step and two step animation added

--- assets/Script/PlayerController.ts
+++ assets/Script/PlayerController.ts
@@ -9,28 +9,66 @@ export class Character extends Component {
 
     // [2]
     // @property
-    // serializableDummy = 0;
-    private _startJump = false;
+    // for fake tween
+    private _startJump: boolean = false;
     private _jumpStep: number = 0;
-    private _curlJumpTime:number = 0;
+    private _curJumpTime: number = 0;
     private _jumpTime: number = 0.1;
-    private _curlJumpSpeed: number = 0;
-    private _curlPos: Vec3 = new Vec3();
-    private _deltaPos: Vec3 = new Vec3(0,0,0);
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = new Vec3();
+    private _deltaPos: Vec3 = new Vec3(0, 0, 0);
     private _targetPos: Vec3 = new Vec3();
     private _isMoving = false;
+
     start () {
-        systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this)
+        // Your initialization goes here.
+        systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
     }
 
-    onMouseUp(event: EventMouse){
-        if(event.getButton() === 0){
-            
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
+
+    }
+
+    jumpByStep(step: number) {
+        if (this._isMoving) {
+            return;
+        }
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep, 0, 0));
+
+        this._isMoving = true;
+    }
+
+    onOnceJumpEnd() {
+        this._isMoving = false;
+    }
+
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+                this.onOnceJumpEnd();
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
         }
     }
-    // update (deltaTime: number) {
-    //     // [4]
-    // }
 }
 
 /**
